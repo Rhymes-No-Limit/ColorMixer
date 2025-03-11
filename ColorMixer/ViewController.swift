f5123039aa93bfb255b366b0347ad9e77fa0ce68
Andrey <Rhymes.un.limit@gmail.com>
2025-03-11 15:53:42 +0300

    Prepare project

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  ColorMixer
-//
-//  Created by Андрей Терентьев on 11.03.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
 
