645890d6b37968d26993e872e2ea2202c1484d2b
Andrey <Rhymes.un.limit@gmail.com>
2025-05-01 18:54:37 +0300

    connected Ui with code

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -2,12 +2,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
-
+    @IBAction func redSliderAction(_ sender: UISlider) {
+    }
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+    }
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+    }
+    
 
 }
 
